93559b1f02f9dd4828a23db15fcdac6611cc63ee
Xiaoyu Zhang <xiaoyu@csusm.edu>
2025-05-18 09:27:26 -0700

    add .sh files

---
+++ play15.sh
@@ -0,0 +1,1 @@
+python human_play.py --model_type resnet --model_file res_best_policy_15_15_5.model --width 15
---
+++ play8.sh
@@ -0,0 +1,1 @@
+python human_play.py --model_type pytorch --model_file best.model4 --width 8
